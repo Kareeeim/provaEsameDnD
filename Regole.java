2632030812327b8977ba6032e64ed578711c9eef
Kareeeim <karimrafai9@gmail.com>
2026-01-30 17:29:52 +0100

    0.0.2

--- Regole.java
+++ Regole.java
@@ -1,13 +1,13 @@
 package provaEsameDnD;
 
 public class Regole {
-    public static final double VITA_MAX = 100;
-    public static final double DANNO_BASE = 30;
-    public static final double TIRO_SALVEZZA = 10;
+    protected static final double VITA_MAX = 100;
+    protected static final double DANNO_BASE = 30;
+    protected static final double TIRO_SALVEZZA = 10;
  
     // regole di attacco
-    public static final double dimezzaAttaccoSubitoGuerriero = 2;
-    public static final double sneakAttackLadro = 2.5;
-    public static final double interventoDivino = 1.5;
-    public static final double pallaDiFuoco = 100;
+    protected static final double dimezzaAttaccoSubitoGuerriero = 2;
+    protected static final double sneakAttackLadro = 2.5;
+    protected static final double interventoDivino = 1.5;
+    protected static final double pallaDiFuoco = 100;
 }
